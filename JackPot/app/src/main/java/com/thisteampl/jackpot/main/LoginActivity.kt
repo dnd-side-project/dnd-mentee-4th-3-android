07cd1bf896816aefd1dcb247be58da7b1bef4d9b
kimyunseok <hk1554@naver.com>
2021-01-21 06:03:19 +0900

    #development 2
    add Naver Login

--- JackPot/app/src/main/java/com/thisteampl/jackpot/main/LoginActivity.kt
+++ JackPot/app/src/main/java/com/thisteampl/jackpot/main/LoginActivity.kt
@@ -9,10 +9,13 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.model.AuthErrorCause.*
-import com.kakao.sdk.user.UserApiClient
+import com.nhn.android.naverlogin.OAuthLogin
+import com.nhn.android.naverlogin.OAuthLoginHandler
 import com.thisteampl.jackpot.R
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONException
+import org.json.JSONObject
+
 
 /*
 * 카카오 로그인 기능 구현 : https://studyforcoding.tistory.com/6?category=800442 참조.
@@ -23,6 +26,8 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var mOAuthLoginInstance : OAuthLogin // 네이버 로그인 모듈
+
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,61 +36,59 @@ class LoginActivity : AppCompatActivity() {
         setupView()
     }
 
+    // 카카오 로그인을 위한 callback 메서드.
     val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         /* error 가 null이 아니라면 로그인 불가.*/
         if (error != null) {
-            when {
-                error.toString() == AccessDenied.toString() -> {
-                    Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
-                }
-                error.toString() == InvalidClient.toString() -> {
-                    Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
-                }
-                error.toString() == InvalidGrant.toString() -> {
-                    Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
-                }
-                error.toString() == InvalidRequest.toString() -> {
-                    Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
-                }
-                error.toString() == InvalidScope.toString() -> {
-                    Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
-                }
-                error.toString() == Misconfigured.toString() -> {
-                    Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
-                }
-                error.toString() == ServerError.toString() -> {
-                    Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
-                }
-                error.toString() == Unauthorized.toString() -> {
-                    Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
-                }
-                else -> { // Unknown
-                    Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
-                }
-            }
+            Toast.makeText(this, "카카오 로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
         }
         /*token이 null이 아니라면 카카오 API로 값을 불러와서 회원의 정보를 가져온다.
         * 그리고 회원가입 페이지로 이동한다.*/
         else if (token != null) {
-            Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, SignUpActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java).putExtra("signuptype", 1)
             startActivity(intent)
             finish()
+        }
+    }
+
+    // 네이버 로그인을 위해 토큰을 받아옴. 안드로이드에서는 스레드를 사용해서 웹의 서비스에 접근해야 한다.
+    //추가로 id와 name을 받아와서 다음 activity에 extra로 넘겨준다.
+    private val naverOAuthLoginHandler: OAuthLoginHandler = object : OAuthLoginHandler() {
+        override fun run(success: Boolean) {
+            if (success) {
+
+                Thread {
+                    val accessToken: String = mOAuthLoginInstance.getAccessToken(baseContext)
+                    val data: String = mOAuthLoginInstance.requestApi(baseContext, accessToken, "https://openapi.naver.com/v1/nid/me")
+                    var id: String = ""
+                    var name: String = ""
+
+                    try {
+                        id = JSONObject(data).getJSONObject("response").getString("id")
+                        name = JSONObject(data).getJSONObject("response").getString("name")
+                    }
+                    catch (e: JSONException) { }
+                    val intent = Intent(baseContext, SignUpActivity::class.java)
+                        .putExtra("signuptype", 2).putExtra("id", id).putExtra("name", name)
+                    startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+                    finish()
+                }.start()
+            } else {
+                Toast.makeText(baseContext, "네이버 로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     // 화면이 구성되고 View를 만들어 준다.
     private fun setupView(){
 
-        // 카카오 자체에서 지원해 주는 API, 로그인이 되어 있다면 바로 회원가입 화면으로 간다.
-        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
-                if (tokenInfo != null) {
-                    Toast.makeText(this, "로그인이 돼 있습니다. 회원가입 화면으로 갑니다.", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, SignUpActivity::class.java)
-                    startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
-                    finish()
-            }
-        }
+        mOAuthLoginInstance = OAuthLogin.getInstance()
+        mOAuthLoginInstance.init( // 로그인 모듈 초기화
+            this,
+            "RQcA9ncoO8aUd2YwNz1o",
+            "v0NYcGwVmY" ,
+            "잭팟"
+        )
 
         //카카오로 로그인 버튼의 기능. callback 함수를 호출해서 회원가입 페이지로 이동하게 한다.
         login_kakao_login_button.setOnClickListener {
@@ -97,9 +100,25 @@ class LoginActivity : AppCompatActivity() {
         }
 
         login_signup_button.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java).putExtra("signuptype", 0)
+            startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+            finish()
+        }
+
+        login_naver_login_button.setOnClickListener{
+
+            mOAuthLoginInstance.startOauthLoginActivity(this, naverOAuthLoginHandler)
+        }
+
+        login_login_button.setOnClickListener {
+            //서버와 연동해서 아이디와 비밀번호가 일치하는게 있는지 확인하는 코드
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
             finish()
         }
     }
+
 }
+
+
+
--- JackPot/app/src/main/java/com/thisteampl/jackpot/main/SignUpActivity.kt
+++ JackPot/app/src/main/java/com/thisteampl/jackpot/main/SignUpActivity.kt
@@ -18,38 +18,53 @@ class SignUpActivity : AppCompatActivity() {
         setupView()
     }
 
-    //만일 메인화면에서 넘어온 상태로, SNS 로그인이 돼 있다면 메인화면으로 간다.(기능구현예정)
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+        finish()
+    }
+
+    //만일 메인화면에서 넘어온 상태로, 회원가입이 된 상태라면 메인화면으로 간다.(기능구현예정)
 
     // 화면이 구성되고 View를 만들어 준다.
     private fun setupView(){
 
         /* 유저 정보에 저장해 둘 3개 SNS의 idx들*/
-        var kakaoidx:Long? = 0
-        var googleidx:Long? = 0
-        var naveridx:Long? = 0
+        var signUpType: Int = intent.getIntExtra("signuptype", 0)
+        // 회원가입 타입, 0 : 일반회원가입, 1 : 카카오 로그인, 2 : 네이버 로그인, 3 : 구글 로그인
+        var userIdx: Long? = 0
 
-        //카카오 로그인을 했을 시 카카오idx와 이름을 불러온다.
-        UserApiClient.instance.me { user, error ->
-            if (error == null) {
-                kakaoidx = user?.id
+        if(signUpType == 1) {
+            Toast.makeText(this, "카카오 로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+            //카카오 로그인을 했을 시 카카오idx와 이름을 불러온다.
+            UserApiClient.instance.me { user, error ->
+                userIdx = user?.id
+                signup_id_text.setText(userIdx.toString())
                 signup_name_text.setText("${user?.kakaoAccount?.profile?.nickname}")
             }
+        } else if(signUpType == 2) {
+            Toast.makeText(this, "네이버 로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+            //네이버 로그인을 했을 시 네이버idx와 이름을 불러온다.
+            var id: String? = intent.getStringExtra("id")
+            var name: String? = intent.getStringExtra("name")
+            userIdx = id?.toLong()
+            signup_id_text.setText(userIdx.toString())
+            signup_name_text.setText(name)
+        } else if(signUpType == 3) {
+            //구글 로그인을 했을 시 구글idx와 이름을 불러온다.
         }
 
         signup_cancel_button.setOnClickListener {
-
-            // 취소 버튼 눌렀을 시  KAKAO 로그아웃.
-            UserApiClient.instance.logout { error ->
-                if (error == null) {
-                    Toast.makeText(this, "카카오 계정이 로그아웃 됐습니다.", Toast.LENGTH_SHORT).show()
-                }
-            }
-
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            finish()
         }
 
         signup_finish_button.setOnClickListener{
+            /*
+            * 서버에 유저정보를 insert하는 코드
+            * */
             Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
