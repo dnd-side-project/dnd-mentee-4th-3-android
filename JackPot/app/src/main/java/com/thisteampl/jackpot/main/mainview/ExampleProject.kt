9616c04efe8feb49eca1ef327e2c5f54457e069b
이경창 <lkc263@naver.com>
2021-01-22 18:15:23 +0900

    change mainmenu & input project

---
+++ JackPot/app/src/main/java/com/thisteampl/jackpot/main/mainview/ExampleProject.kt
@@ -0,0 +1,12 @@
+package com.thisteampl.jackpot.main.mainview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thisteampl.jackpot.R
+
+class ExampleProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_project)
+    }
+}
