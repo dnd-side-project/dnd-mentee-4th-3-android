904cfbbc11173c1c3db84c6d6816271d7f00b47e
kimyunseok <hk1554@naver.com>
2021-02-02 17:23:56 +0900

    feat. add stack & tool button onclick method

--- JackPot/app/src/main/java/com/thisteampl/jackpot/main/SignUpActivity.kt
+++ JackPot/app/src/main/java/com/thisteampl/jackpot/main/SignUpActivity.kt
@@ -1,22 +1,14 @@
 package com.thisteampl.jackpot.main
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.text.Editable
-import android.text.InputFilter
 import android.text.TextWatcher
-import android.util.Log
-import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.widget.addTextChangedListener
 import com.kakao.sdk.user.UserApiClient
 import com.thisteampl.jackpot.R
 import kotlinx.android.synthetic.main.activity_signup.*
@@ -30,7 +22,8 @@ class SignUpActivity : AppCompatActivity() {
 
     var positionBtn = arrayOfNulls<Button>(3)
     var stateBtn = arrayOfNulls<Button>(3)
-    var sGrageBtn = arrayOfNulls<Button>(4)
+    var sGradeBtn = arrayOfNulls<Button>(4)
+    val stackTool = mutableListOf<String>()
 
     var emailCheck: Boolean = false
     var page: Int = 0
@@ -78,7 +71,6 @@ class SignUpActivity : AppCompatActivity() {
 
     // 화면이 구성되고 View를 만들어 준다.
     private fun setupView(){
-
         var signUpType: Int = intent.getIntExtra("signuptype", 0)
         // 회원가입 타입, 0 : 일반회원가입, 1 : 카카오 로그인, 2 : 네이버 로그인, 3 : 구글 로그인
         var token: String = intent.getStringExtra("token").toString()
@@ -86,12 +78,14 @@ class SignUpActivity : AppCompatActivity() {
 
             0 -> {
                 signup_email_signup_layout.visibility = View.VISIBLE
+                signup_page_viewer.text = "$page / 6"
             }
             1 -> {
                 Toast.makeText(this, "카카오 로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
                 //카카오 로그인을 했을 시 카카오idx와 이름을 불러온다.
                 signup_nickname_layout.visibility = View.VISIBLE
                 page = 1
+                signup_page_viewer.text = "$page / 6"
                 UserApiClient.instance.me { user, error ->
                     signup_name_text.setText(token)
                     //signup_name_text.setText("${user?.kakaoAccount?.profile?.nickname}")
@@ -103,6 +97,7 @@ class SignUpActivity : AppCompatActivity() {
                 //네이버 로그인을 했을 시 네이버idx와 이름을 불러온다.
                 signup_nickname_layout.visibility = View.VISIBLE
                 page = 1
+                signup_page_viewer.text = "$page / 6"
                 signup_name_text.setText(token)
 
             }
@@ -111,6 +106,7 @@ class SignUpActivity : AppCompatActivity() {
                 //구글 로그인을 했을 시 구글idx와 이름을 불러온다.
                 signup_nickname_layout.visibility = View.VISIBLE
                 page = 1
+                signup_page_viewer.text = "$page / 6"
                 signup_name_text.setText(token)
             }
             // 지역 선택을 해 줄 배열과 액티비티의 스피너와 연결해줄 어댑터.
@@ -140,6 +136,7 @@ class SignUpActivity : AppCompatActivity() {
                     signup_nickname_layout.visibility = View.VISIBLE
                     signup_previous_button.visibility = View.VISIBLE
                 }
+                signup_page_viewer.text = "$page / 6"
             } else if(page == 1) {
                 if(signup_name_text.text.trim().length < 3 || signup_name_text.text.trim().length > 6) {
                     Toast.makeText(this, "닉네임은 최소 3글자 최대 6글자 입니다.", Toast.LENGTH_SHORT).show()
@@ -151,6 +148,7 @@ class SignUpActivity : AppCompatActivity() {
                     signup_call_name_position_text.text =
                         signup_name_text.text.toString() + "님, 어떤\n포지션에 해당하시나요?"
                 }
+                signup_page_viewer.text = "$page / 6"
             } else if(page == 2) {
                 if(region == "지역") {
                     Toast.makeText(this, "지역을 선택해주세요.", Toast.LENGTH_SHORT).show()
@@ -159,6 +157,7 @@ class SignUpActivity : AppCompatActivity() {
                     signup_region_layout.visibility = View.GONE
                     signup_position_layout.visibility = View.VISIBLE
                 }
+                signup_page_viewer.text = "$page / 6"
             } else if(page == 3) {
                 if(position == "직군") {
                     Toast.makeText(this, "직군을 선택해주세요.", Toast.LENGTH_SHORT).show()
@@ -170,6 +169,7 @@ class SignUpActivity : AppCompatActivity() {
                 if(state[0] == '학' && state[1] == '생') {
                     signup_state_grade_layout.visibility = View.VISIBLE // 학생일때 보이기 (이전갔다가 돌아올 때)
                 }
+                signup_page_viewer.text = "$page / 6"
             } else if(page == 4) {
                 when (state) {
                     "상태" -> {
@@ -198,6 +198,8 @@ class SignUpActivity : AppCompatActivity() {
                         }
                     }
                 }
+                signup_page_viewer.text = "$page / 6"
+                signup_write_later_button.visibility = View.VISIBLE
             } else if(page == 5) {
                 page = 6
                 if(position == "개발자") {
@@ -206,8 +208,10 @@ class SignUpActivity : AppCompatActivity() {
                     signup_designer_tool_layout.visibility = View.GONE
                 }
                 signup_introduce_layout.visibility = View.VISIBLE
+                signup_page_viewer.text = "$page / 6"
             } else if(page == 6) {
                 page = 7
+                signup_page_viewer.visibility = View.GONE
                 signup_introduce_layout.visibility = View.GONE
                 signup_open_layout.visibility = View.VISIBLE
 
@@ -226,6 +230,7 @@ class SignUpActivity : AppCompatActivity() {
                 signup_previous_button.visibility = View.GONE
                 signup_email_signup_layout.visibility = View.VISIBLE
                 signup_nickname_layout.visibility = View.GONE
+                signup_page_viewer.text = "$page / 6"
             }else if(page == 2) {
                 if(signUpType != 0) {
                     signup_previous_button.visibility = View.GONE
@@ -233,10 +238,12 @@ class SignUpActivity : AppCompatActivity() {
                 page = 1
                 signup_nickname_layout.visibility = View.VISIBLE
                 signup_region_layout.visibility = View.GONE
+                signup_page_viewer.text = "$page / 6"
             }else if(page == 3) {
                 page = 2
                 signup_region_layout.visibility = View.VISIBLE
                 signup_position_layout.visibility = View.GONE
+                signup_page_viewer.text = "$page / 6"
             }else if(page == 4) {
                 page = 3
                 signup_position_layout.visibility = View.VISIBLE
@@ -244,8 +251,10 @@ class SignUpActivity : AppCompatActivity() {
                 if(state[0] == '학' && state[1] == '생') {
                     signup_state_grade_layout.visibility = View.GONE // 학생일때 가리기
                 }
+                signup_page_viewer.text = "$page / 6"
             }else if(page == 5) {
                 page = 4
+                signup_write_later_button.visibility = View.GONE
                 signup_state_layout.visibility = View.VISIBLE
                 if(position == "개발자") {
                     signup_developer_stack_layout.visibility = View.GONE
@@ -255,6 +264,7 @@ class SignUpActivity : AppCompatActivity() {
                 if(state[0] == '학' && state[1] == '생') {
                     signup_state_grade_layout.visibility = View.VISIBLE // 학생일때 보이기
                 }
+                signup_page_viewer.text = "$page / 6"
             }else if(page == 6) {
                 page = 5
                 when (position) {
@@ -273,6 +283,7 @@ class SignUpActivity : AppCompatActivity() {
                     }
                 }
                 signup_introduce_layout.visibility = View.GONE
+                signup_page_viewer.text = "$page / 6"
             }else if(page == 7) { // 프로필 공개 여부.
                 Toast.makeText(this, "회원가입이 완료되었습니다.\n프로필을 다른 사람이 확인할 수 없습니다.", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
@@ -329,32 +340,37 @@ class SignUpActivity : AppCompatActivity() {
         stateBtn[2]?.setOnClickListener { stateBtn[2]?.let { it1 -> this.btnOnClick(it1) }}
 
         //학생 학년 선택 리스너
-        sGrageBtn[0] = findViewById(R.id.signup_student_grade_one)
-        sGrageBtn[1] = findViewById(R.id.signup_student_grade_two)
-        sGrageBtn[2] = findViewById(R.id.signup_student_grade_three)
-        sGrageBtn[3] = findViewById(R.id.signup_student_grade_four)
-        sGrageBtn[0]?.setOnClickListener { sGrageBtn[0]?.let { it1 -> this.btnOnClick(it1) } }
-        sGrageBtn[1]?.setOnClickListener {sGrageBtn[1]?.let { it1 -> this.btnOnClick(it1) } }
-        sGrageBtn[2]?.setOnClickListener { sGrageBtn[2]?.let { it1 -> this.btnOnClick(it1) }}
-        sGrageBtn[3]?.setOnClickListener { sGrageBtn[3]?.let { it1 -> this.btnOnClick(it1) }}
+        sGradeBtn[0] = findViewById(R.id.signup_student_grade_one)
+        sGradeBtn[1] = findViewById(R.id.signup_student_grade_two)
+        sGradeBtn[2] = findViewById(R.id.signup_student_grade_three)
+        sGradeBtn[3] = findViewById(R.id.signup_student_grade_four)
+        sGradeBtn[0]?.setOnClickListener { sGradeBtn[0]?.let { it1 -> this.btnOnClick(it1) } }
+        sGradeBtn[1]?.setOnClickListener {sGradeBtn[1]?.let { it1 -> this.btnOnClick(it1) } }
+        sGradeBtn[2]?.setOnClickListener { sGradeBtn[2]?.let { it1 -> this.btnOnClick(it1) }}
+        sGradeBtn[3]?.setOnClickListener { sGradeBtn[3]?.let { it1 -> this.btnOnClick(it1) }}
+
     }
 
     private fun btnOnClick(v : View) {
         var id = v.id
+
+        // 직군 선택 버튼의 리스너 설정
         if(id == R.id.signup_designer_button || id == R.id.signup_developer_button
             || id == R.id.signup_director_button) {
             var pos = Integer.parseInt(v.contentDescription.toString())
             for(i in 0..2) {
                 if(i == pos) {
+                    setStackToolBtn()
                     positionBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity,R.drawable.radius_background_transparent_select)
                     position = positionBtn[i]?.text.toString()
                 }
                 else {
-                    positionBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent)
+                    positionBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
                 }
             }
         }
 
+        // 상태 선택 버튼의 리스너 설정
         if(id == R.id.signup_state_student || id == R.id.signup_state_jobfinder
             || id == R.id.signup_state_junior) {
             var pos = Integer.parseInt(v.contentDescription.toString())
@@ -366,29 +382,118 @@ class SignUpActivity : AppCompatActivity() {
                         signup_state_grade_layout.visibility = View.VISIBLE
                     } else {
                         signup_state_grade_layout.visibility = View.GONE
-                        sGrageBtn[0]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent)
-                        sGrageBtn[1]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent)
-                        sGrageBtn[2]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent)
-                        sGrageBtn[3]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent)
+                        sGradeBtn[0]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                        sGradeBtn[1]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                        sGradeBtn[2]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                        sGradeBtn[3]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
                     }
                 }
                 else {
-                    stateBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent)
+                    stateBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
                 }
             }
         }
 
+        // 학년 선택 버튼의 리스너 설정
         if(id == R.id.signup_student_grade_one || id == R.id.signup_student_grade_two
             || id == R.id.signup_student_grade_three || id == R.id.signup_student_grade_four) {
             var pos = Integer.parseInt(v.contentDescription.toString())
             for(i in 0..3) {
                 if(i == pos) {
-                    sGrageBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity,R.drawable.radius_background_transparent_select)
-                    state = "학생 " + sGrageBtn[i]?.text.toString()
+                    sGradeBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity,R.drawable.radius_background_transparent_select)
+                    state = "학생 " + sGradeBtn[i]?.text.toString()
                 }
                 else {
-                    sGrageBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent)
+                    sGradeBtn[i]?.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
                 }
+            }
+        }
+    }
+
+    // 스택, 툴 버튼의 온클릭 리스너
+    private fun stackToolBtnOnClick(button: Button) {
+        if(!stackTool.contains(button.text.toString())) {
+            button.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_background_transparent_select)
+            stackTool.add(button.text.toString())
+        } else {
+            button.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+            stackTool.remove(button.text.toString())
+        }
+    }
+
+    /*
+    * 스택, 툴 버튼을 설정해 주는 메서드, 그리고 스택, 툴을 저장해주는 리스트를 초기화해준다.
+    * 스택 레이아웃을 통째로 받아와서 getChild를 해서 버튼들에만 입력해주면 훨씬 더 코드가 간결해질 것 같다.
+    * */
+    private fun setStackToolBtn(){
+        stackTool.clear()
+
+        for (i in 0 until signup_developer_stack_layout_first.childCount) {
+            val child: View = signup_developer_stack_layout_first.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+
+        for (i in 0 until signup_developer_stack_layout_second.childCount) {
+            val child: View = signup_developer_stack_layout_second.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+
+        for (i in 0 until signup_developer_stack_layout_third.childCount) {
+            val child: View = signup_developer_stack_layout_third.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+
+        for (i in 0 until signup_developer_stack_layout_fourth.childCount) {
+            val child: View = signup_developer_stack_layout_fourth.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+
+        for (i in 0 until signup_designer_tool_layout_first.childCount) {
+            val child: View = signup_designer_tool_layout_first.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+
+        for (i in 0 until signup_designer_tool_layout_second.childCount) {
+            val child: View = signup_designer_tool_layout_second.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+        for (i in 0 until signup_designer_tool_layout_third.childCount) {
+            val child: View = signup_designer_tool_layout_third.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+        for (i in 0 until signup_designer_tool_layout_fourth.childCount) {
+            val child: View = signup_designer_tool_layout_fourth.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
+            }
+        }
+        for (i in 0 until signup_designer_tool_layout_fifth.childCount) {
+            val child: View = signup_designer_tool_layout_fifth.getChildAt(i)
+            if(child is Button) {
+                child.background = ContextCompat.getDrawable(this@SignUpActivity, R.drawable.radius_button_effect)
+                child.setOnClickListener { this.stackToolBtnOnClick(child) }
             }
         }
     }
