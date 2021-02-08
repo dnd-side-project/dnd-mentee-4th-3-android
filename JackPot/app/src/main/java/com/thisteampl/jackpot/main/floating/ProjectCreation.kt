9a995b26293422182ddb511ff4c041edfc70e83e
이경창 <lkc263@naver.com>
2021-02-09 01:24:48 +0900

    Input xml, Write project recruitment

--- JackPot/app/src/main/java/com/thisteampl/jackpot/main/floating/ProjectCreation.kt
+++ JackPot/app/src/main/java/com/thisteampl/jackpot/main/floating/ProjectCreation.kt
@@ -1,12 +1,18 @@
 package com.thisteampl.jackpot.main.floating
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.thisteampl.jackpot.R
+import kotlinx.android.synthetic.main.activity_project_creation.*
 
 class ProjectCreation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project_creation)
+
+        createproject_minusbutton_imageview.setOnClickListener{
+            finish()
+        }
     }
 }
