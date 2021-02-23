ee4642c41e680bf63909f3b09508b123089562ff
kimyunseok <hk1554@naver.com>
2021-02-24 04:25:32 +0900

    feat. add Reload MainActivity after write project

--- JackPot/app/src/main/java/com/thisteampl/jackpot/main/floating/ProjectCreation.kt
+++ JackPot/app/src/main/java/com/thisteampl/jackpot/main/floating/ProjectCreation.kt
@@ -1,5 +1,6 @@
 package com.thisteampl.jackpot.main.floating
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,8 +9,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.thisteampl.jackpot.R
+import com.thisteampl.jackpot.main.MainActivity
 import com.thisteampl.jackpot.main.projectController.ProjectGetElement
 import com.thisteampl.jackpot.main.projectController.projectAPI
+import com.thisteampl.jackpot.main.userpage.ProfileActivity
 import kotlinx.android.synthetic.main.activity_project_creation.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -788,9 +791,9 @@ class ProjectCreation : AppCompatActivity() {
 
                         // 데이터 전달하지 못했다면
                         if(response.isSuccessful){
-                            ToastmakeTextPrint("프로젝트 모집글 작성 완료 되었습니다.")
+                            ToastmakeTextPrint("프로젝트 모집글 작성이 완료 되었습니다.")
                         }else{
-                            ToastmakeTextPrint("프로젝트 모집글 작성 완료되지 않았습니다.")
+                            ToastmakeTextPrint("프로젝트 모집글 작성이 완료되지 않았습니다.")
                             Log.d("tag","${response.code().toString()}")
                             Log.e("tag","onFailure" + response.message())
                         }
@@ -798,6 +801,8 @@ class ProjectCreation : AppCompatActivity() {
                 })
 
             finish()
+            val intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
 
         }
     }
