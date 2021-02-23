430285c5575141952f70c5055a42de8e480c0bb4
kimyunseok <hk1554@naver.com>
2021-02-24 07:40:45 +0900

    fix. Setting : change Setting UI

--- JackPot/app/src/main/java/com/thisteampl/jackpot/main/setting/SettingActivity.kt
+++ JackPot/app/src/main/java/com/thisteampl/jackpot/main/setting/SettingActivity.kt
@@ -2,17 +2,25 @@ package com.thisteampl.jackpot.main.setting
 
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.thisteampl.jackpot.R
 
 class SettingActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
-    class MainSettingFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.settings_preference, rootKey)
+    class MainSettingFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return inflater.inflate(R.layout.fragment_setting_main, container, false)
         }
     }
 
@@ -20,6 +28,7 @@ class SettingActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.setting_container, MainSettingFragment())
